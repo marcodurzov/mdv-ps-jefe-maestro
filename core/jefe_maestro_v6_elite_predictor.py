bad909a295aa33bdccc84460822b5da255991e43
marcodurzov <marco.durzov@gmail.com>
2026-02-23 15:05:01 -0600

    Update jefe_maestro_v6_elite_predictor.py

--- core/jefe_maestro_v6_elite_predictor.py
+++ core/jefe_maestro_v6_elite_predictor.py
@@ -1,12 +1,18 @@
-pandas
-numpy
-scikit-learn
-scipy
-matplotlib
-requests
-openpyxl
-gspread
-google-auth
-google-auth-oauthlib
-google-auth-httplib2
-psutil
+import pandas as pd
+import numpy as np
+import psutil
+import requests
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+import os
+import sys
+import math
+import random
+import datetime
